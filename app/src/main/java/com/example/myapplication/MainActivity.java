22c4ef32ddfb539d4ceae7b0eb758f1031ffef88
Hosen Nes <husen43nas4@gmail.com>
2024-11-02 20:51:39 +0200

    MY FIRST

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,6 +4,8 @@ import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,24 +14,32 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText etWelcom;
+    private EditText etName, etYear;
+    private TextView tvShowDetails;
+    private String name,details;
+    private int year;
 
-    @SuppressLint("MissingInflatedId")
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
-        etWelcom = findViewById(R.id.textView);
-    }
-
-
-    public void change_text(View view){
-        etWelcom.setText("this is my first app");
+        etName = findViewById(R.id.etName);
+        etYear = findViewById(R.id.etYear);
+        tvShowDetails = findViewById(R.id.tvShowDetails);
 
     }
-    public void finishApp(View view){
-        finish();
-        System.exit(0);
+
+    public void shoDetails(View view) {
+        if(etName.getText().toString().isEmpty() || etYear.getText().toString().isEmpty())
+        {
+            Toast.makeText(this, "pleas fill in fieled.. ", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            name= etName.getText().toString();
+            year= Integer.parseInt(etYear.getText().toString());
+            int age = 2024 -year;
+            details ="welcome "+ name+"\nYour age is: "+age;
+            tvShowDetails.setText(details);
+        }
     }
 }
